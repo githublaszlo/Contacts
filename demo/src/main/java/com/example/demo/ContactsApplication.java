e05711a6c0decc4f8c0e69078b35a0f18409b843
Kis László <szemelyes.kl@gmail.com>
2022-10-29 13:24:25 +0200

    Person kiegészítése és kiírás próba

--- demo/src/main/java/com/example/demo/ContactsApplication.java
+++ demo/src/main/java/com/example/demo/ContactsApplication.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
+import com.example.demo.person.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class ContactsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactsApplication.class, args);
 	}
 
+	@GetMapping
+	public List<Person> hello() {
+			return List.of(
+					new Person(
+							1L,
+							"Alex",
+							1L,
+							2L
+					)
+			);
+
+
+	}
 }
--- demo/src/main/java/com/example/demo/person/Person.java
+++ demo/src/main/java/com/example/demo/person/Person.java
@@ -17,6 +17,38 @@ public class Person {
         this.temporaryAddressId = temporaryAddressId;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPermanentAddressId() {
+        return permanentAddressId;
+    }
+
+    public void setPermanentAddressId(Long permanentAddressId) {
+        this.permanentAddressId = permanentAddressId;
+    }
+
+    public Long getTemporaryAddressId() {
+        return temporaryAddressId;
+    }
+
+    public void setTemporaryAddressId(Long temporaryAddressId) {
+        this.temporaryAddressId = temporaryAddressId;
+    }
+
     @Override
     public String toString() {
         final StringBuffer sb = new StringBuffer("Person{");
