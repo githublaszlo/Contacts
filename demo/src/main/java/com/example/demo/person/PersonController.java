b83d90334ec620918c6416b6cdc5be0960859b1c
Kis László <szemelyes.kl@gmail.com>
2022-10-29 16:32:14 +0200

    Person-ban PUT method megvalósítása

--- demo/src/main/java/com/example/demo/person/PersonController.java
+++ demo/src/main/java/com/example/demo/person/PersonController.java
@@ -32,4 +32,16 @@ public class PersonController {
         personService.deletePerson(id);
 
     }
+
+    // pl.: PUT http://localhost:8080/api/v2/person/1?name=Béla
+    // változtatja meg az 1-es id-val rendelkező személy nevét
+    @PutMapping(path = "{personId}")
+    public void updatePerson(
+            @PathVariable("personId") Long personId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) Long permanentAddressId,
+            @RequestParam(required = false) Long temporaryAddressId){
+        personService.updatePerson(personId, name, permanentAddressId, temporaryAddressId);
+    }
+
 }
--- demo/src/main/java/com/example/demo/person/PersonService.java
+++ demo/src/main/java/com/example/demo/person/PersonService.java
@@ -3,7 +3,9 @@ package com.example.demo.person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -36,5 +38,40 @@ public class PersonService {
                     "person with id " + personId + " does not exists");
         }
         personRepository.deleteById(personId);
+    }
+
+    @Transactional
+    public void updatePerson(Long personId,
+                             String name,
+                             Long permanentAddressId,
+                             Long temporaryAddressId) {
+        Person person = personRepository.findById(personId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "person with id " + personId + " does not exist"));
+
+        if (name != null &&
+        name.length() > 0 &&
+        !Objects.equals(person.getName(), name)) {
+            Optional<Person> personOptional = personRepository.findPersonByName(name);
+            if (personOptional.isPresent()){
+                throw new IllegalStateException("name taken");
+            }
+            person.setName(name);
+        }
+
+        // megnézni, hogy van-e adott id-val cím.
+        boolean permAddressExist = true;
+        if (permanentAddressId != null && permAddressExist &&
+        !Objects.equals(person.getPermanentAddressId(), permanentAddressId)) {
+            person.setPermanentAddressId(permanentAddressId);
+        }
+
+        // megnézni, hogy van-e adott id-val cím.
+        boolean tempAddressExist = true;
+        if (temporaryAddressId != null && tempAddressExist &&
+                !Objects.equals(person.getTemporaryAddressId(), temporaryAddressId)) {
+            person.setTemporaryAddressId(temporaryAddressId);
+        }
+
     }
 }
